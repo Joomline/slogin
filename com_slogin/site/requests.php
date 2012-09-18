cb355426d9c7359a8ba090c9a872e01e86c58446
Arkadiy <a.sedelnikov@gmail.com>
2012-09-19 00:02:51 +0700

    Добавил файл с возвращаемыми данными.

---
+++ com_slogin/site/requests.php
@@ -0,0 +1,82 @@
+
+mail
+
+Array
+    (
+    [0] => stdClass Object
+(
+    [pic_50] => http://avt.appsmail.ru/mail/arkadiy.sedelnikov/_avatar50
+            [friends_count] => 0
+            [pic_22] => http://avt.appsmail.ru/mail/arkadiy.sedelnikov/_avatar22
+            [nick] => Аркадий Седельников
+            [is_verified] => 0
+            [is_online] => 0
+            [pic_big] => http://avt.appsmail.ru/mail/arkadiy.sedelnikov/_avatarbig
+            [last_name] => Седельников
+            [has_pic] => 0
+            [email] => arkadiy.sedelnikov@mail.ru
+            [pic_190] => http://avt.appsmail.ru/mail/arkadiy.sedelnikov/_avatar190
+            [referer_id] =>
+            [vip] => 0
+            [pic_32] => http://avt.appsmail.ru/mail/arkadiy.sedelnikov/_avatar32
+            [birthday] =>
+            [referer_type] =>
+            [link] => http://my.mail.ru/mail/arkadiy.sedelnikov/
+            [uid] => 12715093441520828426
+            [app_installed] => 1
+            [pic_128] => http://avt.appsmail.ru/mail/arkadiy.sedelnikov/_avatar128
+            [sex] => 0
+            [pic] => http://avt.appsmail.ru/mail/arkadiy.sedelnikov/_avatar
+            [pic_small] => http://avt.appsmail.ru/mail/arkadiy.sedelnikov/_avatarsmall
+            [pic_180] => http://avt.appsmail.ru/mail/arkadiy.sedelnikov/_avatar180
+            [first_name] => Аркадий
+            [pic_40] => http://avt.appsmail.ru/mail/arkadiy.sedelnikov/_avatar40
+        )
+
+)
+
+vk
+
+stdClass Object
+(
+[uid] => 176675943
+[first_name] => Аркадий
+[last_name] => Сед
+[nickname] =>
+[mobile_phone] =>
+[home_phone] =>
+)
+
+google
+
+stdClass Object
+(
+[id] => 112217272973802620922
+[email] => a.sedelnikov@gmail.com
+[verified_email] => 1
+[name] => Аркадий Седельников
+[given_name] => Аркадий
+[family_name] => Седельников
+[link] => https://plus.google.com/112217272973802620922
+[picture] => https://lh3.googleusercontent.com/-YRHR-cWwCjM/AAAAAAAAAAI/AAAAAAAAABc/LTCH8IJk5nM/photo.jpg
+[gender] => male
+[birthday] => 0000-12-26
+[locale] => ru
+)
+
+facebook
+
+stdClass Object
+(
+[id] => 100004327660291
+[name] => Arkady  Sedelnikov
+[first_name] => Arkady
+[last_name] => Sedelnikov
+[link] => http://www.facebook.com/profile.php?id=100004327660291
+[gender] => male
+[email] => a.sedelnikov@gmail.com
+[timezone] => 7
+[locale] => ru_RU
+[updated_time] => 2012-09-18T15:44:54+0000
+)
+
