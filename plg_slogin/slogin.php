3d9f2888b1018f15b67ea05c978d416e3695c9dd
Arkadiy <a.sedelnikov@gmail.com>
2012-09-21 09:44:04 +0700

    Плагин удаления зааписей пользователя из таблицы #__slogin_users при удалении пользователя.

---
+++ plg_slogin/slogin.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Social Login
+ *
+ * @version 	1.0
+ * @author		Arkadiy, Joomline
+ * @copyright	© 2012. All rights reserved.
+ * @license 	GNU/GPL v.3 or later.
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+
+class plgUserSlogin extends JPlugin
+{
+	/**
+	 * Remove all sessions for the user name
+	 *
+	 * Method is called after user data is deleted from the database
+	 *
+	 * @param	array		$user	Holds the user data
+	 * @param	boolean		$succes	True if user was succesfully stored in the database
+	 * @param	string		$msg	Message
+	 *
+	 * @return	boolean
+	 * @since	1.6
+	 */
+	public function onUserAfterDelete($user, $succes, $msg)
+	{
+		if (!$succes) {
+			return false;
+		}
+
+		$db = JFactory::getDbo();
+		$db->setQuery(
+			'DELETE FROM '.$db->quoteName('#__slogin_users') .
+			' WHERE '.$db->quoteName('user_id').' = '.(int) $user['id']
+		);
+		$db->Query();
+		return true;
+	}
+}
