d91f6bab1db25c2afa523727f4ca43d8af75e480
Arkadiy <a.sedelnikov@gmail.com>
2013-08-29 11:07:09 +0700

    Добавление плагина профиля пользователя, перемещение папок плагинов.

---
+++ plugins/slogin_integration/profile/helper.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Social Login Integration Plugin Profile
+ *
+ * @version 	1.0
+ * @author		Arkadiy, Joomline
+ * @copyright	© 2013. All rights reserved.
+ * @license 	GNU/GPL v.3 or later.
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+
+class plgProfileHelper
+{
+	public static function getProfile($userid)
+    {
+        $db = JFactory::getDbo();
+        $q = $db->getQuery(true);
+        $q->select('*');
+        $q->from('#__plg_slogin_profile');
+        $q->where('`user_id` = '.(int)$userid);
+        $q->where('`current_profile` = 1');
+        $db->setQuery($q);
+        $profile = $db->loadObject();
+		if (!$profile)
+            return false;
+		//Получаем папку с изображениями
+		$plugin = JPluginHelper::getPlugin('slogin_integration', 'slogin_avatar');
+		$pluginParams = new JRegistry();
+		$pluginParams->loadString($plugin->params);
+		$paramFolder = $pluginParams->get('rootfolder', 'images/avatar');
+		//обратная совместимость со старыми версиями
+        $profile->avatar = preg_replace("/.*?\//","",$profile->avatar);
+		//путь до аватара
+        $profile->avatar = $paramFolder.'/'.$profile->avatar;
+		return $profile;
+	}
+}
+	
+?>
---
+++ plugins/slogin_integration/profile/sql/install.mysql.utf8.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS `jos_plg_slogin_profile` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `slogin_id` varchar(150) NOT NULL,
+  `provider` varchar(100) NOT NULL,
+  `gender` int(1) NOT NULL DEFAULT '0',
+  `f_name` varchar(100) NOT NULL,
+  `l_name` varchar(100) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `mobil_phone` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `social_profile_link` varchar(250) NOT NULL,
+  `country` varchar(5) NOT NULL,
+  `region` varchar(50) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `avatar` varchar(100) NOT NULL,
+  `current_profile` int(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `user_prov` (`user_id`,`provider`),
+  KEY `user_curr` (`user_id`,`current_profile`),
+  KEY `user` (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
