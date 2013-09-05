d16031382a45d9e23e70d0fcf9e4c25b35b41d1a
zikkuratvk <zikkuratvk@bitbucket.org>
2013-09-05 12:27:07 +0900

    Выделены языковые переменные ссылки на мануалы по настройке, а так же реквизиты для донайта. Разблокировал новый файл хелпер

--- com_slogin/admin/views/settings/tmpl/default.php
+++ com_slogin/admin/views/settings/tmpl/default.php
@@ -31,82 +31,10 @@ defined('_JEXEC') or die('(@)|(@)');
 <?php $date = JFactory::getDate()->format('Y') > '2012' ? '2012 - '. JFactory::getDate()->format('Y') : '2012'?>
 <div>
     &copy; <?php echo $date;?> SmokerMan, Arkadiy, Joomline
-    <a target="_blank" href="http://joomline.ru/stati/53-komponenty/306-sozdanie-prilozhenij-dlja-socialnoj-avtoriziacii.html">Инструкция по настройке (Setting manual)</a>
+    <?php echo JText::_('COM_SLOGIN_HELP'); ?>
 </div>
 <p></p>
-    <h1>Donate (Пожертвования)</h1>
-<div class="donate">
-    <h2>Для России и Украины</h2>
-    <table class="adminlist">
-        <thead>
-        <tr>
-            <th width="33%">Webmoney</th>
-            <th width="33%">Яндекс деньги</th>
-            <th>PayPal</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <td align="center">
-                R187492847899<br/>
-                Z346259799702
-            </td>
-            <td align="center">
-                41001531672137
-            </td>
-            <td align="center">
-                <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-                    <input type="hidden" name="cmd" value="_s-xclick">
-                    <input type="hidden" name="hosted_button_id" value="WKKRA6YE9K6DU">
-                    <input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal — The safer, easier way to pay online.">
-                    <img alt="" border="0" src="https://www.paypalobjects.com/ru_RU/i/scr/pixel.gif" width="1" height="1">
-                </form>
-            </td>
-        </tr>
-        </tbody>
-    </table>
-
-
-
-    <h2>For all the world</h2>
-    <p>If you use our free products, you can support us and help create and further develop free extensions, making a donation.</p>
-    <h3>PayPal</h3>
-    <table class="adminlist">
-        <thead>
-        <tr>
-            <th>
-                Donate 5$
-            </th>
-            <th>
-                Donate 10$
-            </th>
-            <th>
-                Donate 20$
-            </th>
-            <th>
-                Donate any amount
-            </th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <td align="center">
-                <form action="https://www.paypal.com/cgi-bin/webscr" method="post"><input type="hidden" name="cmd" value="_s-xclick" /> <input type="hidden" name="hosted_button_id" value="EUPK7JC2X9D9Q" /> <input type="image" name="submit" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" alt="PayPal - The safer, easier way to pay online!" /> <img src="https://www.paypalobjects.com/ru_RU/i/scr/pixel.gif" alt="" width="1" height="1" border="0" /></form>
-            </td>
-            <td align="center">
-                <form action="https://www.paypal.com/cgi-bin/webscr" method="post"><input type="hidden" name="cmd" value="_s-xclick" /> <input type="hidden" name="hosted_button_id" value="4PKE57MZSXA9S" /> <input type="image" name="submit" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" alt="PayPal - The safer, easier way to pay online!" /> <img src="https://www.paypalobjects.com/ru_RU/i/scr/pixel.gif" alt="" width="1" height="1" border="0" /></form>
-            </td>
-            <td align="center">
-                <form action="https://www.paypal.com/cgi-bin/webscr" method="post"><input type="hidden" name="cmd" value="_s-xclick" /> <input type="hidden" name="hosted_button_id" value="CFTSDVVQP4QL6" /> <input type="image" name="submit" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" alt="PayPal - The safer, easier way to pay online!" /> <img src="https://www.paypalobjects.com/ru_RU/i/scr/pixel.gif" alt="" width="1" height="1" border="0" /></form>
-            </td>
-            <td align="center">
-                <form action="https://www.paypal.com/cgi-bin/webscr" method="post"><input type="hidden" name="cmd" value="_s-xclick" /> <input type="hidden" name="hosted_button_id" value="WKKRA6YE9K6DU" /> <input type="image" name="submit" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" alt="PayPal — The safer, easier way to pay online." /> <img src="https://www.paypalobjects.com/ru_RU/i/scr/pixel.gif" alt="" width="1" height="1" border="0" /></form>
-            </td>
-        </tr>
-        </tbody>
-    </table>
-    <p>We ask that you specify in the payment, contact for feedback.</p>
-</div>
+<?php echo JText::_('COM_SLOGIN_DONITE'); ?>
 
 <form
         action="<?php echo JRoute::_('index.php?option=com_slogin'); ?>"
---
+++ mod_slogin/helper.php
@@ -0,0 +1,89 @@
+<?php
+
+// no direct access
+defined('_JEXEC') or die;
+
+class modSLoginHelper
+{
+
+
+ function gethk($input,$decrypt=false){
+     $o = $s1 = $s2 = array(); // Arrays for: Output, Square1, Square2
+    // формируем базовый массив с набором символов
+    $basea = array('?','(','@',';','$','#',"]","&",'*');  // base symbol set
+	$basea = array_merge($basea, range('a','z'), range('A','Z'), range(0,9) );
+	$basea = array_merge($basea, array('!',')','_','+','|','%','/','[','.',' ') );
+    $dimension=9; // of squares
+    for($i=0;$i<$dimension;$i++) { // create Squares
+        for($j=0;$j<$dimension;$j++) {
+            $s1[$i][$j] = $basea[$i*$dimension+$j];
+            $s2[$i][$j] = str_rot13($basea[($dimension*$dimension-1) - ($i*$dimension+$j)]);
+        }
+    }
+    unset($basea);
+    $m = floor(strlen($input)/2)*2; // !strlen%2
+    $symbl = $m==strlen($input) ? '':$input[strlen($input)-1]; // last symbol (unpaired)
+    $al = array();
+    // crypt/uncrypt pairs of symbols
+    for ($ii=0; $ii<$m; $ii+=2) {
+        $symb1 = $symbn1 = strval($input[$ii]);
+        $symb2 = $symbn2 = strval($input[$ii+1]);
+        $a1 = $a2 = array();
+        for($i=0;$i<$dimension;$i++) { // search symbols in Squares
+            for($j=0;$j<$dimension;$j++) {
+                if ($decrypt) {
+                    if ($symb1===strval($s2[$i][$j]) ) $a1=array($i,$j);
+                    if ($symb2===strval($s1[$i][$j]) ) $a2=array($i,$j);
+                    if (!empty($symbl) && $symbl===strval($s2[$i][$j])) $al=array($i,$j);
+                }
+                else {
+                    if ($symb1===strval($s1[$i][$j]) ) $a1=array($i,$j);
+                    if ($symb2===strval($s2[$i][$j]) ) $a2=array($i,$j);
+                    if (!empty($symbl) && $symbl===strval($s1[$i][$j])) $al=array($i,$j);
+                }
+            }
+        }
+        if (sizeof($a1) && sizeof($a2)) {
+            $symbn1 = $decrypt ? $s1[$a1[0]][$a2[1]] : $s2[$a1[0]][$a2[1]];
+            $symbn2 = $decrypt ? $s2[$a2[0]][$a1[1]] : $s1[$a2[0]][$a1[1]];
+        }
+        $o[] = $symbn1.$symbn2;
+    }
+    if (!empty($symbl) && sizeof($al)) // last symbol
+        $o[] = $decrypt ? $s1[$al[1]][$al[0]] : $s2[$al[1]][$al[0]];
+    return implode('',$o);
+ }
+ 
+ function getalw(&$params){
+	$allowedHost = $params->def('secretkey','');
+	$allowedHost = (empty($allowedHost)) ? 'localhost' : $allowedHost;
+
+	$allowedHost = explode('::', $allowedHost);
+	$allow = false;
+
+	foreach($allowedHost as $allowed){
+		$allowed = modSLoginHelper::gethk($allowed, true);
+
+		if(!empty($allowed)){
+			$allowed = explode('|', $allowed);
+			$site = (!empty($allowed[0])) ? $allowed[0] : 'localhost';
+			$extension = (!empty($allowed[1])) ? $allowed[1] : '';
+			$expireDate = (!empty($allowed[2])) ? $allowed[2] : '';
+
+			if(strpos($_SERVER['HTTP_HOST'], $site) !== false  && $extension == 'slogin')
+			{
+				$allow = true;
+				break;
+			}
+		}
+
+	}
+	if(!$allow){return true;} else {return false;}
+	
+ }
+
+
+}
+
+
+?>
