715a8053d9621b875995b1ede5eb05264fa829d3
zikkuratvk <vadim@joomline.ru>
2015-12-24 16:05:47 +0200

    обновление версии

--- mod_slogin/mod_slogin.php
+++ mod_slogin/mod_slogin.php
@@ -2,7 +2,7 @@
 /**
  * Social Login
  *
- * @version 	1.7
+ * @version 	1.9.0
  * @author		SmokerMan, Arkadiy, Joomline
  * @copyright	© 2012. All rights reserved.
  * @license 	GNU/GPL v.3 or later.
